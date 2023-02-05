bda05233ea8995e444a06ac0568baa4c9798e100
Weten-Ogen <alphaoware@gmail.com>
2023-02-05 12:48:24 +0000

    finished working on the Minimum Variable Product for the HomePage

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './Common/Navbar'
+import {BrowserRouter as  Router, Routes, Route} from 'react-router-dom'
+import Home from './Pages/Home/Home';
+import Project from './Pages/Project/Project';
+import Contact from './Pages/Contact/Contact';
+import './main.css'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div className='app-container'>
+    <Router>
+    <Navbar/>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/projects" element={<Project/>}/>
+          <Route path="/contact" element={<Contact/>}/>
+        </Routes>
+    </Router> 
+   </div>
+
   );
 }
 
