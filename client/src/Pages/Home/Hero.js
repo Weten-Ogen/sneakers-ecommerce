58caa7598067882d8f9e10ce2ca02a32b09f65da
Weten-Ogen <alphaoware@gmail.com>
2023-02-08 02:14:22 +0000

    added the images and banners

--- client/src/Pages/Home/Hero.js
+++ client/src/Pages/Home/Hero.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import image from '../../Images/marc.png';
-import { useLocation } from 'react-router-dom';
+import { about_banner } from '../../Images';
+
 
 const Hero = () => {
   return (
@@ -8,8 +8,8 @@ const Hero = () => {
         <div className='hero-message'>
             <div className='hero-info'>
                 <p className='text-1'>my name is <span className='hero-highlight'>marcus gideon oware</span></p>
-                <p className='text-2'>i am a  <span className='hero-highlight'>software enginer</span> in ghana, west africa</p>
-                <p className='text-3'>I have 4 years experience in <span className='hero-highlight'>software enginer</span></p>
+                <p className='text-2'>i am a  <span className='hero-highlight'>software engineer</span> in ghana, west africa</p>
+                <p className='text-3'>I have 4 years experience in <span className='hero-highlight'>software engineer</span></p>
             </div>
             <div className='hero-btns'>
                 <button type='button'>projects</button>
@@ -18,7 +18,7 @@ const Hero = () => {
                 </div>
             </div>
         </div>
-      <img src={image} alt='marcus oware' className='hero-tag hero-image'/>
+      <img src={about_banner} alt='marcus oware' className='hero-tag hero-image'/>
     </article>
   )
 }
