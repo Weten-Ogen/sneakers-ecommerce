d1e46e3a48b363d6c713e8f41706abb05f202cdc
Marcus G. Oware <90935734+Weten-Ogen@users.noreply.github.com>
2023-06-01 12:01:26 +0000

    Update NavProf.jsx

--- src/Components/Navbar/NavProf.jsx
+++ src/Components/Navbar/NavProf.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import avatar from '../../images/image-avatar.png'
-import { BsCart } from 'react-icons/bs'
+
 
 function NavProf({amount}) {
 
 
   return (
     <div className='flex'>
-      <BsCart className='text-black my-6 max-w-[50px]' />
       <p className='text-[rgb(255,167,4)] text-sm  my-3 '>{amount}</p>
       <img src={avatar} className='object-cover h-[70px] py-4 px-0 md:h-[80px] ' alt='avatar'/>
       
